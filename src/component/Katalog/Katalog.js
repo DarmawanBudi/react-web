a7d2dbe71b34c23b9ef2108e0603add573d6b133
darmawan budi <drmwn.budi@gmail.com>
2021-02-03 11:53:28 +0700

    update

--- src/component/Katalog/Katalog.js
+++ src/component/Katalog/Katalog.js
@@ -10,22 +10,23 @@ function Katalog() {
                 {
                     Produk.map((data) => {
                         return (
-                            <div className="col-lg-3 col-md-4 col-sm-6">
+                            <div className="col-lg-3 col-md-4 col-sm-6 col-6">
                                 <Card>
-                                    
-                                    <Card.Body>
                                     <div className="cardImg">
-                                        <Card.Img className="mt-2 img" variant="top" height="200" src="https://i.picsum.photos/id/1031/5446/3063.jpg?hmac=Zg0Vd3Bb7byzpvy-vv-fCffBW9EDp1coIbBFdEjeQWE" />
+                                        <Card.Img className="mt-2 img" variant="top" src="https://i.picsum.photos/id/1031/5446/3063.jpg?hmac=Zg0Vd3Bb7byzpvy-vv-fCffBW9EDp1coIbBFdEjeQWE" />
                                     </div>
-                                        <Card.Title></Card.Title>
+
+                                    <Card.Body>
+                                    
+                                        {/* <Card.Title></Card.Title> */}
                                         <Card.Text>
                                             <b>{data.nama}</b> <br />
                                             {data.keterangan}
                                         </Card.Text>
                                     </Card.Body>
                                     <Card.Footer type="button" className="d-flex justify-content-between">
-                                        <b>Rp {data.harga}.000</b>
-                                        <h3><Button className="btn btn-lg badge badge-pill btn-success">Beli</Button></h3>
+                                        <p>Rp {data.harga}.000</p>
+                                        <h5><Button className="btn btn-lg badge badge-pill btn-success">Beli</Button></h5>
                                     </Card.Footer>
                                 </Card>
                             </div>
