af60530d418aaaa6c758858fe493780f74b8a59d
darmawan budi <drmwn.budi@gmail.com>
2021-02-04 14:17:46 +0700

    update

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import facebook from "./images/facebook.png";
+import instagram from "./images/instagram.png";
+import twitter from "./images/twitter.png";
 
 function Contact() {
     return (
-        <div className="text-center">
-            <div><i class="fab fa-facebook-square fa-4x"></i></div><br></br>
-            <div><i class="fab fa-instagram fa-4x"></i></div><br></br>
-            <div><i class="fab fa-twitter fa-4x"></i></div>
+        <div className="mt-5 text-center">
+            <div>
+                <img src={facebook} />
+            </div>
+
+            <br></br>
+
+            <div>
+                <img src={instagram} />
+            </div>
+
+            <br></br>
+
+            <div>
+                <img src={twitter} />
+            </div>
         </div>
     )
 }
