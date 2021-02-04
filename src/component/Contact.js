a63f31e372e2952870eea093f93eda5d8cdc7534
darmawan budi <drmwn.budi@gmail.com>
2021-02-04 14:32:03 +0700

    update

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import whatsapp from "./images/whatsapp.png";
+import email from "./images/email.png";
 import facebook from "./images/facebook.png";
 import instagram from "./images/instagram.png";
 import twitter from "./images/twitter.png";
@@ -7,17 +9,17 @@ function Contact() {
     return (
         <div className="mt-5 text-center">
             <div>
+                <img src={whatsapp} />
+            </div><br />
+            <div>
+                <img src={email} />
+            </div><br />
+            <div>
                 <img src={facebook} />
-            </div>
-
-            <br></br>
-
+            </div><br />
             <div>
                 <img src={instagram} />
-            </div>
-
-            <br></br>
-
+            </div><br />
             <div>
                 <img src={twitter} />
             </div>
